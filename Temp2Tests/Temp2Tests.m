199f5c6ae2c630f113d980c0786bf15dccdc0789
zard0 <kunzhu_lin@hotmail.com>
2017-07-17 09:37:22 +0800

    设置一个会failed的测试用例

--- Temp2Tests/Temp2Tests.m
+++ Temp2Tests/Temp2Tests.m
@@ -34,7 +34,7 @@
 - (void)testGoodJobShoudNotEqual0 {
     YTLayoutRule *rule = [[YTLayoutRule alloc] init];
     NSInteger result = [rule goodJob];
-    XCTAssertTrue(result, @"The goodJob method should not equal 0");
+    XCTAssertFalse(result, @"The goodJob method should not equal 0");
 }
 
 - (void)testPerformanceExample {
